9ff5cb8a488de5c3a7d60ce0dc0ba905a13353f1
Jval8202 <creppyyy2332@gmail.com>
2026-04-30 13:46:56 -0400

    laptop practice 04 30 2026, the list now has a function called reder

---
+++ logic_for_a_list_that_displays_and_deletes.js
@@ -0,0 +1,78 @@
+// input values
+let form = document.querySelector("#form");
+let input = document.querySelector("#item");
+
+//displayers
+let Values_2_display = document.querySelector("#list");
+//end of displayers
+// buttons
+let submitButton = document.querySelector("#btn");
+submitButton.disabled = true
+
+//arrays
+let list = []
+
+//timer
+let timer;
+
+//functions
+function render(){
+
+    Values_2_display.innerHTML = "";
+
+
+
+
+    list.forEach((item,index)=>{
+
+        //HTML list elements
+        let li = document.createElement("li");
+
+        li.textContent = item;
+
+        let delete_button = document.createElement("button")
+        delete_button.textContent = "Delete";
+
+        delete_button.addEventListener("click",()=>{
+
+            list.splice(index,1);
+
+            render();
+
+        });
+
+        li.appendChild(delete_button);
+
+        Values_2_display.appendChild(li);
+
+    });
+
+}
+
+input.addEventListener("input", (chars) =>{
+    if(chars.target.value !== ""){
+        submitButton.disabled = false
+    }
+    else{
+        submitButton.disabled = true
+    }
+})
+
+form.addEventListener("submit",(event)=>{
+
+    event.preventDefault();
+
+    let word = input.value.trim();
+
+    if(word !== ""){
+
+        list.push(word);
+
+        input.value = "";
+
+        render()
+    }
+
+});
+
+
